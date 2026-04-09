2242e99ba2f7597d72ab6731aaff18ea8ee81d16
Cristian Haunsen <cristianhaunsen@gmail.com>
2026-04-09 10:32:09 -0300

    Validate form only for post request

--- src/Http/HtmxFormRequest.php
+++ src/Http/HtmxFormRequest.php
@@ -5,9 +5,34 @@ declare(strict_types=1);
 namespace Mauricius\LaravelHtmx\Http;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Foundation\Precognition;
 use Mauricius\LaravelHtmx\Http\Concerns\HasHtmxRequest;
 
 class HtmxFormRequest extends FormRequest
 {
     use HasHtmxRequest;
+
+    public function validateResolved()
+    {
+        // Do only validate htmx requests
+        if ($this->isMethod('post')) {
+            $this->prepareForValidation();
+
+            if (! $this->passesAuthorization()) {
+                $this->failedAuthorization();
+            }
+
+            $instance = $this->getValidatorInstance();
+
+            if ($this->isPrecognitive()) {
+                $instance->after(Precognition::afterValidationHook($this));
+            }
+
+            if ($instance->fails()) {
+                $this->failedValidation($instance);
+            }
+
+            $this->passedValidation();
+        }
+    }
 }
